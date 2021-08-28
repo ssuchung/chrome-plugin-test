68f3cdac375057288cee5d0a4498a0d5f88619dc
ssuchung <89036635+ssuchung@users.noreply.github.com>
2021-08-29 01:37:06 +0800

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -21,22 +21,28 @@
 // })
 
 //tab = getCurrentTabId();
-function testOutput(){
-    console.log("......popup.js OUTPUT....");
-};
+// function testOutput(){
+//     console.log("......popup.js OUTPUT....");
+// };
+
+// function startRefresh(){
+//     chrome.browserAction.setIcon({
+//         // 又是一个坑，图标大小不能超了，见文档：
+//         // https://developer.chrome.com/extensions/manifest/icons
+//         path: {
+//             "16": "images/icon-off16.png",
+//             "48": "images/icon-off48.png",
+//             "128": "images/icon-off128.png"
+//         }
+//     })
+// }
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status == "complete") {
         console.log("A new tabId " + tabId);
         console.log(changeInfo);
-        url = new URL(tab.url);
+        const url = new URL(tab.url);
         console.log("host is " + url.hostname);
-         chrome.storage.sync.clear();
-        // // 蠢得可以的语法，用变量作为OBJ的key
-        // hostnameObj = {};
-        // hostnameObj[url.hostname] = true;
-        // console.log(hostnameObj);
-        // chrome.storage.sync.set(hostnameObj);
         chrome.storage.sync.get(url.hostname, (items) => {
             if (items === {}) {
                 chrome.browserAction.setIcon({
@@ -64,13 +70,50 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // if (chrome.storage.sync.get(url.hostname)) {
     //     console.log("storage is true");
     // }
+function tabUpdater(tab){
+    // const url = new URL(tab.url);
+    return () => {
+        console.log("updater")
+        chrome.tabs.update(tab.id,{url:tab.url});
+    };
+}
 
 chrome.browserAction.onClicked.addListener((tab) => {
-    
-  });
+    var url = new URL(tab.url);
+    chrome.storage.sync.get(url.hostname, (items) => {
+        if (items === {}) {
+            chrome.browserAction.setIcon({
+                path: {
+                    "16": "images/icon-on16.png",
+                    "48": "images/icon-on48.png",
+                    "128": "images/icon-on128.png"
+                }
+            });
+            const intV = setInterval(tabUpdater(tab), 2000);
+            chrome.storage.sync.set({[url.hostname]: intV});
+        }
+        else {
+            chrome.browserAction.setIcon({
+                path: {
+                    "16": "images/icon-on16.png",
+                    "48": "images/icon-on48.png",
+                    "128": "images/icon-on128.png"
+                }
+            });
+            clearInterval(items);
+            chrome.storage.sync.remove(url.hostname, () => {
+                console.log("remove successful")
+            });
+        }
+    });
+});
 
 //   chrome.tabs.update(tab.id,{url:tab.url});
 
-
-
+// // 蠢得可以的语法，用变量作为OBJ的key
+        // hostnameObj = {};
+        // hostnameObj[url.hostname] = true;
+        // console.log(hostnameObj);
+        // chrome.storage.sync.set(hostnameObj);
+        // chrome.storage.sync.clear();
  
