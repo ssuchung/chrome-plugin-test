9d04d92420e476cccc2b985149c0b0568ac8e0b4
ssuchung <thsywy@126.com>
2021-09-12 02:16:40 +0800

    is it final?

--- js/background.js
+++ js/background.js
@@ -1,7 +1,14 @@
+// 常驻后台的 background.js
+
+// 正在自动刷新的 tab 的 refreshInfo 数组
 var refInfoTable = [];
-// {data: [{url: xxx， msTime: 60000}]}
+// 保存的 urlData 数组
 var savedData = [];
 
+// 保存在浏览器内的数据是一个 js 对象：
+// {data: [{url: xxx， msTime: 60000}]}
+
+// 切换插件图标，开和关
 function turnOnIcon() {
     chrome.browserAction.setIcon({
         // 又是一个坑，图标大小不能超了，见文档：
@@ -22,6 +29,7 @@ function turnOffIcon() {
         }
     });
 }
+
 function RefreshInfo(tab, msTime = 60000, saved = false) {
     this.tab = tab;
     this.url = new URL(tab.url);
@@ -53,10 +61,8 @@ function sameRefInfoId(refreshInfoA, refreshInfoB) {
     }
 }
 function isRefreshing(tab) {
-    console.log("isRefreshing refInfoTable is ");
     console.log(refInfoTable);
     for (var i = 0; i < refInfoTable.length; i++) {
-        console.log("tabID: " + tab.id + " && refInfoTable[i].tab.id: " + refInfoTable[i].tab.id);
         if (tab.id == refInfoTable[i].tab.id) {
             return true;
         }
@@ -71,6 +77,7 @@ function getRefInfo(tab) {
     }
     return null;
 }
+// 生成用于刷新的函数
 function refresher(refreshInfo) {
     const tab = refreshInfo.tab;
     const tabId = tab.id;
@@ -80,7 +87,7 @@ function refresher(refreshInfo) {
     }
 }
 function popRefInfoFromRT(refreshInfo, func) {
-    // func 是 sameRefInfo sameRefInfoId 之一
+    // func 可以是 sameRefInfo、sameRefInfoId 之一
     var t = [];
     var r;
     for (var i = 0; i < refInfoTable.length; i++) {
@@ -94,19 +101,24 @@ function popRefInfoFromRT(refreshInfo, func) {
     refInfoTable = t;
     return r;
 }
+// 设置自动刷新
 function setRefresh(refreshInfo) {
     const msTime = refreshInfo.msTime;
-    if(isRefreshing(refreshInfo.tab)){
+    if (isRefreshing(refreshInfo.tab)) {
         clearRefresh(getRefInfo(refreshInfo.tab));
     }
+    // setInterval 按时调用刷新函数
+    // 返回值用于 clearInterval 停止按时调用
     refreshInfo.iterval = setInterval(refresher(refreshInfo), msTime);
-    refInfoTable.push(refreshInfo);   
+    refInfoTable.push(refreshInfo);
     turnOnIcon();
+    // 确保 savedData 和浏览器保存的数据一致
     saveUrlData(refreshInfo);
 }
+// 取消自动刷新
 function clearRefresh(refreshInfo) {
     var r = getRefInfo(refreshInfo.tab);
-    if(r){
+    if (r) {
         clearInterval(r.iterval);
         r.iterval = null;
         popRefInfoFromRT(r, sameRefInfoId);
@@ -114,36 +126,38 @@ function clearRefresh(refreshInfo) {
     }
     saveUrlData(refreshInfo);
 }
-function saveUrlData(refreshInfo){
+function saveUrlData(refreshInfo) {
     var urlData = getUrlData(refreshInfo);
     var msTime = refreshInfo.msTime;
-    if(urlData){
-        if(refreshInfo.saved){
+    // 操作 savedData
+    if (urlData) {
+        if (refreshInfo.saved) {
             urlData.msTime = msTime;
         }
-        else{
+        else {
             popUrlData(refreshInfo);
         }
     }
-    else if(refreshInfo.saved){
+    else if (refreshInfo.saved) {
         urlData = new UrlData(refreshInfo.tab.url, msTime);
         savedData.push(urlData);
     }
-    chrome.storage.sync.set({data: savedData}); 
+    // 浏览器保存 savedData
+    chrome.storage.sync.set({ data: savedData });
 }
-function UrlData(url, msTime){
+function UrlData(url, msTime) {
     this.url = url;
     this.msTime = msTime;
 }
-function getUrlData(refreshInfo){
-    for(var i = 0; i < savedData.length; i++){
-        if(refreshInfo.tab.url == savedData[i].url){
+function getUrlData(refreshInfo) {
+    for (var i = 0; i < savedData.length; i++) {
+        if (refreshInfo.tab.url == savedData[i].url) {
             return savedData[i];
         }
     }
     return null;
 }
-function popUrlData(refreshInfo){
+function popUrlData(refreshInfo) {
     var t = [];
     var r;
     var urlData = new UrlData(refreshInfo.tab.url, refreshInfo.msTime);
@@ -158,11 +172,17 @@ function popUrlData(refreshInfo){
     savedData = t;
     return r;
 }
-/********************************************************** */
-// chrome.storage.sync.set({ rT: refInfoTable });
 
+// 以上是定义 
+// 以下是执行
+
+// 打开浏览器窗口时，读取浏览器保存的数据
 chrome.windows.onCreated.addListener((win) => {
+    // key 设为 null 获取所有保存的数据
+    // 虽然实际上只有一个 key，即 data
+    // {data: [{url: xxx， msTime: 60000}]}
     chrome.storage.sync.get(null, (items) => {
+        // js 就是垃圾，不能通过 items === {} 来判定是否空对象
         if (JSON.stringify(items) != '{}') {
             // items 类似 {data: [{url: xxx， msTime: 60000}]}
             savedData = items.data;
@@ -175,23 +195,34 @@ chrome.windows.onCreated.addListener((win) => {
         console.log("refInfoTable is: " + refInfoTable);
     });
 });
+
+// 处理 tab 更新事件，只处理 complete 状态的
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status == "complete") {
-        console.log("tabId: " + tabId + " updated completed");
+        console.log("tabId: " + tabId + " is completed");
         var refreshInfo = new RefreshInfo(tab);
+        // 如果 tab 本来正在自动刷新
+        // 判断更新后的 url 是否原来的 url 
+        // 若不是原来的 url 则取消原来的自动刷新
+        // 再根据 savedData 设置新的自动刷新
+        // 对应的情况就是输入新网址、跳转等情况
         if (isRefreshing(refreshInfo.tab)) {
             console.log("tabId: " + tabId + " is refreshing");
             var oldRefInfo = getRefInfo(tab);
             if (refreshInfo.tab.url != oldRefInfo.tab.url) {
                 clearRefresh(oldRefInfo);
                 var urlData = getUrlData(refreshInfo);
-                if (urlData && urlData.url == refreshInfo.tab.url) {
+                if (urlData) {
                     refreshInfo.msTime = urlData.msTime;
                     refreshInfo.saved = true;
                     setRefresh(refreshInfo);
                 }
             }
+            // 若还是原来的 url 则什么也不用做
+            // 对应的情况就是刷新后 url 不变
         }
+        // 如果 tab 本来没在自动刷新
+        // 根据 savedData 设置新的自动刷新
         else {
             console.log("tabId: " + tabId + " is NOT refreshing");
             var urlData = getUrlData(refreshInfo);
@@ -201,16 +232,20 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
                 setRefresh(refreshInfo);
             }
         }
+        // 根据是否正在自动刷新来切换图标
         chrome.tabs.get(tabId, (tab) => {
-            if(isRefreshing(tab)){
+            if (isRefreshing(tab)) {
                 turnOnIcon();
             }
-            else{
+            else {
                 turnOffIcon();
             }
         });
-    }}
+    }
+}
 );
+
+// 处理在 popup.html 点击确认后的事件
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
         var { action, data } = request;
@@ -224,139 +259,32 @@ chrome.runtime.onMessage.addListener(
         }
     }
 );
-chrome.tabs.onActivated.addListener((activeInfo) =>{
+// 切换 tab 时根据是否正在自动刷新切换图标
+chrome.tabs.onActivated.addListener((activeInfo) => {
     var tabId = activeInfo.tabId;
     chrome.tabs.get(tabId, (tab) => {
-        if(isRefreshing(tab)){
+        if (isRefreshing(tab)) {
             turnOnIcon();
         }
-        else{
+        else {
             turnOffIcon();
         }
     });
 });
-chrome.tabs.onRemoved.addListener((tabId, info) =>{
-    if(isRefreshing({id: tabId})){
-        clearRefresh(getRefInfo({id: tabId}));
+// 关闭 tab 时停止自动刷新
+chrome.tabs.onRemoved.addListener((tabId, info) => {
+    if (isRefreshing({ id: tabId })) {
+        clearRefresh(getRefInfo({ id: tabId }));
     }
 });
 
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     if (changeInfo.status == "complete") {
-//         const url = new URL(tab.url);
-//         const hostname = url.hostname;
-
-//         console.log("New tabId: " + tabId + url);
-//         console.log(changeInfo);
-//         console.log("refInfoTable is ");
-//         console.log(refInfoTable);
-//         if (hostname in refInfoTable){
-//             turnOnIcon();
-//         }
-//         else{
-//             turnOffIcon();
-//         }
-//     }
-// });
 
 
-// chrome.browserAction.onClicked.addListener((tab) => {
-//     var url = new URL(tab.url);
-//     console.log("url click happened" + url.hostname);
-//     chrome.storage.sync.get(url.hostname, (items) => {
-//         console.log("items is ");
-//         console.log(items);
-//         // 垃圾语法，不能通过 items === {} 来判定是否空对象
-//         if (JSON.stringify(items) == '{}') {
-//             console.log("it is off,turn it on");
-//             chrome.browserAction.setIcon({
-//                 path: {
-//                     "16": "images/icon-on16.png",
-//                     "48": "images/icon-on48.png",
-//                     "128": "images/icon-on128.png"
-//                 }
-//             });
-//             items = setInterval(tabUpdater(tab), 2000);
-//             chrome.storage.sync.set({ [url.hostname]: items });
-//             console.log("interval return value is " + items);
-//         }
-//         else {
-//             console.log("it is off")
-//             chrome.browserAction.setIcon({
-//                 path: {
-//                     "16": "images/icon-off16.png",
-//                     "48": "images/icon-off48.png",
-//                     "128": "images/icon-off128.png"
-//                 }
-//             });
-//             // 返回值 items 是一个对象，要获取值，用 []
-//             clearInterval(items[url.hostname]);
-//             console.log("it is isisisisis " + items[url.hostname]);
-//             chrome.storage.sync.remove(url.hostname, () => {
-//                 console.log("remove successful")
-//             });
-//         }
-//     });
-// });
-
-
-
-
-// 获取当前选项卡ID
-// function getCurrentTabId(callback) {
-//     chrome.tabs.query({ active: true, currentWindow: true },
-//         function (tabs) {
-//             if (callback) callback(tabs.length ? tabs[0].id : null);
-//         });
-// }
-
-// $(function () {
-//     var currentTabId = getCurrentTabId();
-//     chrome.storage.sync.set({ currentTabId: currentTabId }, function () {
-//         console.log('保存成功！');
-//     });
-
-//     console.log("实时监控ing");
-
-//     setInterval(function () {
-//         location.reload();
-//     }, 5000);  // 5秒刷新一下
-
-// })
-
-//tab = getCurrentTabId();
-// function testOutput(){
-//     console.log("......popup.js OUTPUT....");
-// };
-
-
-// var urlTable = {};
-// chrome.windows.onCreated.addListener((win) => {
-//     urlTable = chrome.storage.sync.get(null, (items) => {
-//         urlTable = items;
-//     });
-// });
-
-// function getCurrentTab(callback) {
-//     var t;
-//     chrome.tabs.query({ active: true, currentWindow: true },
-//         (tabs) => {
-//             console.log(tabs[0]);
-//             console.log('over');
-//             if (callback && tabs && tabs[0]) {
-//                 callback(tabs[0]);
-//             }
-//             t = tabs[0];
-//         });
-//     return t;
-// }
-
-
-//   chrome.tabs.update(tab.id,{url:tab.url});
-
-// // 蠢得可以的语法，用变量作为OBJ的key
-        // hostnameObj = {};
-        // hostnameObj[url.hostname] = true;
-        // console.log(hostnameObj);
-        // chrome.storage.sync.set(hostnameObj);
-        // chrome.storage.sync.clear();
+// 蠢得可以的语法，用变量作为OBJ的key
+// obj = {};
+// k = "something";
+// obj[k] = true;
+// obj is {"something": true}
+// 或者这样，这个中括号就尼玛莫名奇妙
+// obj = {[k]: true};
+// 哪怕从 Lisp 里面抄个反引号（quote）呢
--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,25 @@
+// 因为 chrome 的安全策略，popup.html 不能混入 js 代码
+// 只能通过 src 引入 js 文件，在 popup.html 头部写：
+// <script type="text/javascript" src="../js/popup.js"></script>
 var btnQd;
 var saveurl;
 var checked;
 var time;
 var bg;
 function btnClick() {
-    var rT = bg.refInfoTable;
+    // 获取当前活动的 tab 
     chrome.tabs.query({ active: true, currentWindow: true },
         (tabs) => {
             var tab = tabs[0];
             var msTime;
             if (time.value != "") {
+                // time.value 是字符串，转换成数字
                 msTime = parseInt(time.value) * 60 * 1000;
             }
             var saved = saveurl.checked;
             var refreshInfo = new bg.RefreshInfo(tab, msTime, saved);
             if (checked.checked) {
+                // 发送消息，和 background.js 进行通信
                 chrome.runtime.sendMessage(
                     { action: "setRefresh", data: refreshInfo },
                     undefined
@@ -34,20 +39,25 @@ function btnClick() {
             }
         }
     );
-    // window.close(); //关闭 popup
+    window.close(); //关闭 popup
 }
 
+// 通过事件，在 popup.html 载入后绑定 Element
 document.addEventListener('DOMContentLoaded', function () {
     btnQd = document.getElementById("btnQd");
+    // 绑定处理点击的函数
     btnQd.onclick = btnClick;
     checked = document.getElementById("autorefresh");
     saveurl = document.getElementById("saveurl");
     time = document.getElementById("timetofresh");
+    // 获取 background.js
     bg = chrome.extension.getBackgroundPage();
+    // 获取当前活动的 tab 
     chrome.tabs.query({ active: true, currentWindow: true },
         (tabs) => {
             var tab = tabs[0];
             var refreshInfo;
+            // 设置 Element 的初始值
             if (bg.isRefreshing(tab)) {
                 refreshInfo = bg.getRefInfo(tab);
                 checked.checked = true;
